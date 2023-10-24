af6fe25936f34809fc934a478dcb2dfcd9dd3352
raziuddin khazi <raziuddin.khazi@alexanderthamm.com>
2023-10-24 17:57:04 +0200

    pip install wheel

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="myproject",
     version="1.0.0",
-    packages=find_packages(),
-    install_requires=[],
+    packages=find_packages("src"),
+    install_requires=["wheel"],
     tests_require=["pytest"],
 )
